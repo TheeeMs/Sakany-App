650fb48809504ddf7bb273ff4518fc56b1679824
Mahmoud SharWeeDa <mahmoudsharweeda9840n@gmail.com>
2026-01-21 20:59:59 +0200

    test4

--- src/screens/about/AboutScreen.tsx
+++ src/screens/about/AboutScreen.tsx
@@ -5,7 +5,7 @@ export default function AboutScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>About Sakany</Text>
       <Text style={styles.description}>
-        Your trusted app for finding the perfect home.
+        Your  app for finding the perfect home.
       </Text>
     </View>
   );
