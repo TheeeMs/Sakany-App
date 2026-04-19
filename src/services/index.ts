81f108db1ef6fbe979aba1aa84eab4bea747d936
Amr3011 <amroosamafakharany@gmail.com>
2026-04-19 18:37:02 +0200

    refactor: remove unused API service exports from index file

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,1 @@
-export { api, getApiBaseUrl, setApiAccessToken } from "./api";
-export {
-  getCurrentUser,
-  loginWithPhone,
-  loginWithEmail,
-  register,
-  registerWithEmail,
-  refreshAccessToken,
-  sendOtp,
-  type AuthResponse,
-  type CurrentUser,
-  type LoginPhonePayload,
-  type RegisterPayload,
-  type SendOtpPayload,
-  type SendOtpResponse,
-} from "./auth";
+// API services will go here
